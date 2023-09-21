1658bcb44d6982746128809248e7ca631d60a7f8
nhatto2022 <119521256+nhatto2022@users.noreply.github.com>
2023-09-21 19:58:37 -0400

    including google api speedinsight
    including google api speedinsight

--- Netlify/functions/analyzeSite.js
+++ Netlify/functions/analyzeSite.js
@@ -1,8 +1,18 @@
+const fetch = require('node-fetch');
+
 exports.handler = async function(event, context) {
-    // Your function logic will go here
-    
+    const { websiteURL } = JSON.parse(event.body);
+
+    const API_KEY = process.env.PAGESPEED_API_KEY;  // Store your API key in Netlify's environment variables
+    const endpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${websiteURL}&key=${API_KEY}`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    // Extract necessary information from the data object and send it back
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello from analyzeSite function!" })
+        body: JSON.stringify(data)  // Modify this to extract only necessary details
     };
 }
