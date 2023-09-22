946086ddbfda5b605e9b4ffb395399384524dfa4
nhatto2022 <119521256+nhatto2022@users.noreply.github.com>
2023-09-21 20:14:28 -0400

    fixes CORS issue
    fixes CORS issue

--- Netlify/functions/analyzeSite.js
+++ Netlify/functions/analyzeSite.js
@@ -2,38 +2,29 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
     const { websiteURL } = JSON.parse(event.body);
-    const PAGESPEED_API_KEY = process.env.PAGESPEED_API_KEY;  // Store your PageSpeed API key in Netlify's environment variables
+    const API_KEY = process.env.PAGESPEED_API_KEY;  
+    const endpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${websiteURL}&key=${API_KEY}`;
 
-    const pageSpeedEndpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${websiteURL}&key=${PAGESPEED_API_KEY}`;
-    const mobileFriendlyEndpoint = `https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run?key=${PAGESPEED_API_KEY}`; // We're assuming you're using the same API key for both services.
-
-    // Fetch PageSpeed Insights
-    const pageSpeedResponse = await fetch(pageSpeedEndpoint);
-    const pageSpeedData = await pageSpeedResponse.json();
-
-    // Fetch Mobile Friendly Test
-    const mobileFriendlyResponse = await fetch(mobileFriendlyEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            url: websiteURL
-        })
-    });
-    const mobileFriendlyData = await mobileFriendlyResponse.json();
-
-    // Extract necessary information
-    const speedScore = pageSpeedData.lighthouseResult.categories.performance.score * 100; // Score is between 0 and 1, hence multiplied by 100.
-    const mobileFriendlyStatus = mobileFriendlyData.mobileFriendliness;
-
-    const result = {
-        speedScore: speedScore,
-        mobileFriendly: mobileFriendlyStatus
-    };
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(result)
-    };
-};
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "https://websitebrush.com", 
+                "Access-Control-Allow-Headers": "Content-Type"
+            },
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        console.error("Error fetching from PageSpeed Insights:", error);
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "https://websitebrush.com",
+                "Access-Control-Allow-Headers": "Content-Type"
+            },
+            body: JSON.stringify({ error: "Internal Server Error" })
+        };
+    }
+}
