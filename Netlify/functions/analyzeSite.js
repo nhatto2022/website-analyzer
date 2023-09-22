1672aa62c004e8730552d73de6a6c7dbe729f6bc
nhatto2022 <119521256+nhatto2022@users.noreply.github.com>
2023-09-21 20:21:31 -0400

    update 2 - CORS
    update 2 - CORS

--- Netlify/functions/analyzeSite.js
+++ Netlify/functions/analyzeSite.js
@@ -1,30 +1,42 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
+    if (event.httpMethod !== "POST") {
+        return { 
+            statusCode: 405, 
+            headers: { 
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*" 
+            },
+            body: "Method Not Allowed" 
+        };
+    }
+
     const { websiteURL } = JSON.parse(event.body);
-    const API_KEY = process.env.PAGESPEED_API_KEY;  
+
+    const API_KEY = process.env.PAGESPEED_API_KEY;
     const endpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${websiteURL}&key=${API_KEY}`;
 
     try {
         const response = await fetch(endpoint);
         const data = await response.json();
+
         return {
             statusCode: 200,
             headers: {
-                "Access-Control-Allow-Origin": "https://websitebrush.com", 
-                "Access-Control-Allow-Headers": "Content-Type"
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*" 
             },
             body: JSON.stringify(data)
         };
     } catch (error) {
-        console.error("Error fetching from PageSpeed Insights:", error);
         return {
             statusCode: 500,
             headers: {
-                "Access-Control-Allow-Origin": "https://websitebrush.com",
-                "Access-Control-Allow-Headers": "Content-Type"
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*" 
             },
-            body: JSON.stringify({ error: "Internal Server Error" })
+            body: JSON.stringify({ error: "Failed fetching data from Google PageSpeed Insights." })
         };
     }
-}
+};
